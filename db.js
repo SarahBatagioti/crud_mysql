5f3b3f72d200966011332c912132c310ba60e676
SarahBatagioti <sarah.montuanibt@gmail.com>
2024-09-16 20:05:59 -0300

    criação tabela clientes

--- db.js
+++ db.js
@@ -11,8 +11,17 @@ con.connect(function(erro) {
     if (erro) throw erro
     console.log('Conectado!')
 
-    con.query("CREATE DATABASE crud", function (erro, result) {
+  /*con.query("CREATE DATABASE crud", function (erro, result) {
         if (erro) throw erro
         console.log("Banco de dados Criado!")
-    }) 
+    }) */
+
+    var sql = "CREATE TABLE clientes (id INT NOT NULL AUTO_INCREMENT," +
+              "nome VARCHAR(255) NOT NULL, idade int NOT NULL, uf VARCHAR(20) NOT NULL, PRIMARY KEY(id))"
+
+    con.query(sql, function (erro, result) {
+        if (erro) throw erro
+        console.log("Tabela criada")
+    })
+
 })
