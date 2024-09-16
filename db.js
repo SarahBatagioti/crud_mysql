42a9f7b2400d19b2b547e9eb6a7412d253ac6c37
SarahBatagioti <sarah.montuanibt@gmail.com>
2024-09-16 20:16:25 -0300

    inserção dados tabela clientes

--- db.js
+++ db.js
@@ -16,12 +16,19 @@ con.connect(function(erro) {
         console.log("Banco de dados Criado!")
     }) */
 
-    var sql = "CREATE TABLE clientes (id INT NOT NULL AUTO_INCREMENT," +
+  /*var sql = "CREATE TABLE clientes (id INT NOT NULL AUTO_INCREMENT," +
               "nome VARCHAR(255) NOT NULL, idade int NOT NULL, uf VARCHAR(20) NOT NULL, PRIMARY KEY(id))"
 
-    con.query(sql, function (erro, result) {
+  /*con.query(sql, function (erro, result) {
         if (erro) throw erro
         console.log("Tabela criada")
+    }) */
+
+
+    var sql = "INSERT INTO clientes (id, nome, idade, uf) values (1, 'Sarah', 18, 'SP')";
+    con.query(sql, function (erro, result){
+        if (erro) throw erro
+        console.log("Registro inserido") 
     })
 
 })
