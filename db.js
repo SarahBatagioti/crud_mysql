0df2f5cbdf06275a94926912eb1584d72e9d4691
SarahBatagioti <sarah.montuanibt@gmail.com>
2024-09-16 21:17:35 -0300

    combinar duas tabelas (inner join)

--- db.js
+++ db.js
@@ -24,7 +24,6 @@ con.connect(function(erro) {
     var sql = "CREATE TABLE clientes (id INT NOT NULL AUTO_INCREMENT," +
               "nome VARCHAR(255) NOT NULL, idade int NOT NULL, uf VARCHAR(20) NOT NULL, PRIMARY KEY(id))"
 
-    /*
     con.query(sql, function (erro, result) {
         if (erro) throw erro
         console.log("Tabela criada")
@@ -50,10 +49,39 @@ con.connect(function(erro) {
     */
 
     // deletar registro
+    /*
     var sql = "DELETE FROM clientes WHERE nome = 'Sarah'"
     con.query(sql, function (erro, result){
         if (erro) throw erro
         console.log("Registro deletado!" + result.affectedRows)
     })
+    */
+
+    // para excluir a tabela
+    // DROP TABLE clientes
+
+    // para atualizar registros na tabela
+    // UPDATE clientes SET nome = 'Luiz Claudio' WHERE nome = 'Claudio'
+
+    // limitar o número de regitros exibidos na consulta 
+    // SELECT * FROM clientes LIMIT 5
+
+    // combinar linhas de duas ou mais tabelascom base em uma coluna
+    /*
+    var sql = "CREATE TABLE pedidos (id INT PRIMARY KEY, cliente_id INT, produto VARCHAR(100), quantidade INT, valor_unitario DECIMAL(10, 2), FOREIGN KEY (cliente_id) REFERENCES clientes(id))"
+
+    con.query(sql, function (erro, result) {
+        if (erro) throw erro
+        console.log("Tabela criada")
+    }) 
+    */
+
+    // fazer a ligação com join
+    var sql = "SELECT c.nome, p.produto, p.quantidade, p.valor_unitario FROM clientes c INNER JOIN pedidos p ON c.id = p.cliente_id"
+    con.query(sql, function (erro, result) {
+        if (erro) throw erro
+        console.log("Combinação feita!")
+        console.log(result)
+    }) 
 
 })
